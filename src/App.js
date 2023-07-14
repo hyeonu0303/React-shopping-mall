872a60fc304e5bbc34db988806f2115259bbe722
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-14 22:47:21 +0900

    상세페이지 내용추가

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import {useState} from 'react';
 import {Navbar, Container,Nav,NavDropdown} from 'react-bootstrap';
 import mainBg from './img/bg.png';
 import data from './data.js';
-
+import {Routes, Route, Link} from 'react-router-dom'
 
 function App() {
 
   let [shoes] = useState(data)
   return (
-    <div className="App">
+    <div className="App container">
+      
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Shop</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
@@ -34,20 +35,37 @@ function App() {
       </Navbar.Collapse>
     </Container>
   </Navbar>
-  <div className='main-bg' style={{backgroundImage:`url(${mainBg})`}}></div>
+  <Link to="/">홈</Link>
+  <Link to="/detail">상세페이지</Link>
+  <Routes>
+    <Route path="/" element={
+    <>
+    <div className='main-bg' style={{backgroundImage:`url(${mainBg})`}}></div>
   
-  <div className='container'>
-    <div className='row'>
-      {
-        shoes.map((a,i)=>{
-          return(
-            <Product shoes={shoes} index={i} key={i}/>
-          )
-        })
-      }
+    <div className='container'>
+      <div className='row'>
+        {
+          shoes.map((a,i)=>{
+            return(
+              <Product shoes={shoes} index={i} key={i}/>
+              )
+            })
+          }
+      </div>
     </div>
-  </div>
-  
+    </>
+  }/>
+  </Routes>
+  <Routes>
+  <Route path="/detail" element={
+    <>
+      <Detail shoes={shoes[0]} i={1}/>
+      <Detail shoes={shoes[1]} i={2}/>
+      <Detail shoes={shoes[2]} i={3}/>
+    </>
+    } 
+  />
+  </Routes>
   </div>
 )}
 
@@ -59,6 +77,24 @@ const Product= (props)=>{
         <p>{props.shoes[props.index].content}</p>
         <p>{props.shoes[props.index].price}</p>
     </div>
+  )
+}
+//상세페이지 컴포넌트
+const Detail = (props) => {
+  return(
+  <div className="container">
+  <div className="row">
+    <div className="col-md-6">
+      <img src={`https://codingapple1.github.io/shop/shoes${props.i}.jpg`} width="100%" />
+    </div>
+    <div className="col-md-6">
+      <h4 className="pt-5">{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+      <button className="btn btn-danger">주문하기</button> 
+    </div>
+  </div>
+</div> 
   )
 }
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,18 +1,16 @@
-let data=[
+let data = [
   {
     id : 0,
     title : "White and Black",
     content : "Born in France",
     price : 120000
   },
-
   {
     id : 1,
     title : "Red Knit",
     content : "Born in Seoul",
     price : 110000
   },
-
   {
     id : 2,
     title : "Grey Yordan",
