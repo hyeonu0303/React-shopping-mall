6eb70f5b263054d6443b565160a615cb55657e3c
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-17 22:23:08 +0900

    TAP UI제작

--- src/App.js
+++ src/App.js
@@ -83,6 +83,20 @@ function App() {
   }/>
   </Routes>
   <Routes>
+  <Nav variant="tabs"  defaultActiveKey="link0">
+    <Nav.Item>
+      <Nav.Link eventKey="link0">버튼0</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link1">버튼1</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link2">버튼2</Nav.Link>
+    </Nav.Item>
+</Nav>
+<div>내용0</div>
+<div>내용1</div>
+<div>내용2</div> 
   <Route path="/detail/:id" element={
     <>
       <Detail shoes={shoes}/>
